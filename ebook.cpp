cb0041e7b9ddd7f4054a9c5fd87f4598a61db924
YaShlushka <paderinee@gmail.com>
2025-07-22 16:41:35 +0300

    Add files via upload

---
+++ ebook.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <iomanip>
+
+class BookmarksManager {
+public:
+	void Read(int user, int count) {
+		if(user_to_page_.count(user)) {
+			int old_page = user_to_page_[user];
+
+			if(--users_on_page_[old_page] == 0) {
+				users_on_page_.erase(old_page);
+			}
+		}
+
+		user_to_page_[user] = count;
+		++users_on_page_[count];
+	}
+
+	double Cheer(int user) {
+		if(!user_to_page_.count(user)) {
+			return 0;
+		}
+
+		if(user_to_page_.size() == 1) {
+			return 1;
+		}
+
+		int user_page = user_to_page_[user];
+		int count = 0;
+		for (auto it = users_on_page_.begin(); it != users_on_page_.end() && it->first < user_page; ++it) {
+			count += it->second;
+		}
+
+		return (count * 1.0) / (user_to_page_.size() - 1);
+	}
+
+private:
+	std::map<int, int> users_on_page_;
+	std::unordered_map<int, int> user_to_page_;
+};
+
+int main() {
+	int n;
+	std::cin >> n;
+	BookmarksManager manager;
+
+	for(int i = 0; i < n; ++i) {
+		std::string request_type;
+		std::cin >> request_type;
+
+		if(request_type == "READ") {
+			int user_name;
+			int page;
+			std::cin >> user_name >> page;
+			manager.Read(user_name, page);
+		} else if(request_type == "CHEER") {
+			int user_name;
+			std::cin >> user_name;
+
+			std::cout << std::setprecision(6) << manager.Cheer(user_name) << std::endl;
+		}
+	}
+
+	return 0;
+}
